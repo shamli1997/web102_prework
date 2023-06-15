5b181e82609c01d6875410245bf7ebdea05e5ecf
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-06-15 11:34:18 -0700

    Added event listeners

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ let totalContributions = GAMES_JSON.reduce((total, game) => {
   return total + game.backers;
 }, 0);
 
-console.log(totalContributions);
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
 contributionsCard.innerHTML = totalContributions.toLocaleString("en-US");
 // grab the amount raised card, then use reduce() to find the total amount raised
@@ -95,8 +94,12 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
-
+  let unfundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+  });
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(unfundedGames);
+  console.log(unfundedGames.length);
 }
 
 // show only games that are fully funded
@@ -104,8 +107,12 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
-
+  let fundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged >= game.goal;
+  });
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(fundedGames);
+  console.log(fundedGames.length);
 }
 
 // show all games
@@ -113,6 +120,7 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -121,7 +129,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
  * Skills used: template literals, ternary operator
